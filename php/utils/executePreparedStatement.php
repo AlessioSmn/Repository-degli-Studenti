b753e8c7887bc5f8d45ac25dd6f1f5fcd940940b
Simocnini <alessiosimoncini02@gmail.com>
2024-03-10 15:11:51 +0100

    executePreparedStatement updated, lastInsertRowID returned

--- php/utils/executePreparedStatement.php
+++ php/utils/executePreparedStatement.php
@@ -1,14 +1,15 @@
 <?php
 
 /**
- * Funzione di utlità per processare query / DML statements. Richiede che sia stato precedentemente incluso un file con le variabili di configurazione per la connessione al database
+ * Funzione di utilità per processare query / DML statements. Richiede che sia stato precedentemente incluso un file con le variabili di configurazione per la connessione al database
  * @param string $sqlStatement Query / DML statement che si vuole eseguire 
- * @param int $affectedRows Reference. Stores the number of affected rows
+ * @param int $affectedRows Reference. Memorizza il numero di record modificati / restituiti
  * @param string $parameterTypes Optional. Default = null. Stringa dei tipi dei parametri della query
  * @param array $parameterValue Optional. Default = null. Array di parametri della query
+ * @param array $lastInsertedId Optional. Default = null. Se lo statement è insert o update salva il valore dell'id
  * @return mysqli_result|boolean|string[]
  */
-function executePreparedStatement($sqlStatement, &$affectedRows, $parameterTypes = null, $parameterValue = null){
+function executePreparedStatement($sqlStatement, &$affectedRows, $parameterTypes = null, $parameterValue = null, &$lastInsertedId = null){
       // Connessione con il database, secondo i paramteri specificati in config/config.php
       $connection = mysqli_connect(DB_host, DB_user, DB_pass, DB_name, DB_port);
 
@@ -18,16 +19,20 @@ function executePreparedStatement($sqlStatement, &$affectedRows, $parameterTypes
       $affectedRows = -2;
 
       // Se lo statement non è stato preparato correttamente
-      if(!$statement)
+      if(!$statement){
+            mysqli_close($connection);
             return false;
+      }
 
       // Eseguo la bind di tutti i parametri se sono presenti
       if(!is_null($parameterTypes) && !is_null($parameterValue) && !empty($parameterTypes) && !empty($parameterValue))
             variableBindParam($statement, $parameterTypes, $parameterValue);
 
       // errore in esecuzione
-      if(!$statement->execute())
+      if(!$statement->execute()){
+            mysqli_close($connection);
             return false;
+      }
 
       // Ricavo il result set
       $result = $statement->get_result();
@@ -35,6 +40,11 @@ function executePreparedStatement($sqlStatement, &$affectedRows, $parameterTypes
       // Imposto il numero di righe modificate / ritornate ricavadolo dalle opportune funzioni
       // (Se lo statement è una query richiede mysqli_num_rows, se è un'operazione di DMl richiede getConnection)
       $affectedRows = max(mysqli_affected_rows($connection), $result ? mysqli_num_rows($result) : 0);
+
+      // Ricavo l'ultimo id inserito
+      $lastInsertedId = mysqli_insert_id($connection);
+
+      mysqli_close($connection);
 
       // Restituisco il result set
       return $result;
