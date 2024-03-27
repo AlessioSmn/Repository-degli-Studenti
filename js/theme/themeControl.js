f1d8a3315e3eb6423f35f516659bd2a2f5a1a159
Simocnini <alessiosimoncini02@gmail.com>
2024-03-27 22:52:47 +0100

    Footer logo changed with theme

--- js/theme/themeControl.js
+++ js/theme/themeControl.js
@@ -1,10 +1,14 @@
 
 const CUSTOM_THEME = "_CUSTOM_THEME";
+let brightBackground = false;
 
 // Controllo se è stato impostato un tema nel localStorage
 let theme = window.localStorage.getItem('theme');
 if (theme)
       applyTheme(theme);
+
+// Modifico il logo solo a pagina caricata
+document.addEventListener('DOMContentLoaded', adaptLogo);
 
 /**
  * Imposta il tema in localStorage per mantenere il tema costante tra le varie pagine
@@ -15,6 +19,8 @@ function setTheme(mode){
       window.localStorage.setItem('theme', mode);
 
       applyTheme(mode);
+
+      adaptLogo();
 }
 
 /**
@@ -27,20 +33,28 @@ function applyTheme(theme){
       if(theme == CUSTOM_THEME){
 
             // Prendo le informazioni sul tema dal localStorage
-            const storedCustomTheme = window.localStorage.getItem('customThemeInfo');
+            let storedCustomTheme = window.localStorage.getItem('customThemeInfo');
 
             // Il tema custom potrebbe essere stato richiesto ma mai impostato
             if (!storedCustomTheme)
                   return;
 
             // Deseserializzo le informazioni sul tema
-            const customTheme = JSON.parse(storedCustomTheme);
+            let customTheme = JSON.parse(storedCustomTheme);
 
-            // Scorro su tutte le variabili in customTheme
-            Object.keys(customTheme).forEach(variable => {
-                  // Applica il valore della variabile al documento HTML
-                  document.documentElement.style.setProperty(variable, customTheme[variable]);
-            });
+            // Scorro sui vari campi (le varie varibili --bgColor, --foreColor, etc)
+            for(let themeElement in customTheme){
+
+                  // Ricavo il valore della varibaile, ossia il colore
+                  let themeElementValue = customTheme[themeElement];
+
+                  // Applico il colore
+                  document.documentElement.style.setProperty(themeElement, themeElementValue);
+
+                  if(themeElement == '--bgColor_dark')
+                        brightBackground = isColorBright(themeElementValue);
+
+            }
       }
 
       else{
@@ -50,5 +64,34 @@ function applyTheme(theme){
             // Per i temi predefiniti utilizzo i file css
             themeStylesheet = document.getElementById("theme");
             themeStylesheet.href = "css/themes/" + theme + ".css";
+
+            // Tra quelli default l'unico che necessita il logo scuro è il tema chiaro
+            brightBackground = theme == 'light';
       }
+}
+
+/**
+ * Indica se il colore passato come parametro è luminoso o meno
+ * @param {String} color Colore espresso come #RRGGBB
+ * @param {Number} threshold Soglia (da 0 a 255) per rilevare la lumiosità
+ */
+function isColorBright(color, threshold = 160){
+
+      // 0.299 R + 0.587 G + 0.114 B;
+      let R = parseInt(color.substring(1, 3), 16);
+      let G = parseInt(color.substring(3, 5), 16);
+      let B = parseInt(color.substring(5, 7), 16);
+
+      let brightness = 0.299 * R + 0.587 * G + 0.114 * B;
+
+      return brightness >= threshold;
+}
+
+/**
+ * Seleziona il logo bianco o nero a seconda della luminosità dello sfondo
+ */
+function adaptLogo(){
+      const logoElement = document.getElementById("footerUnipiLogo");
+      const version = brightBackground ? "black" : "white";
+      logoElement.src = "media/.ico/cherubino_" + version + ".ico";
 }
