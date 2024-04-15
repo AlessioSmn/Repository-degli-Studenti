e8878370fb43f65f5e9b4d9c2f3091e08cf98f4a
Simocnini <alessiosimoncini02@gmail.com>
2024-04-15 14:45:17 +0200

    config final

--- config/config.php
+++ config/config.php
@@ -2,10 +2,6 @@
 define('DB_host', 'localhost');
 define('DB_user', 'root');
 define('DB_pass', '');
-define('DB_name', 'pweb');
-define('DB_port', 3308);
-/*
 define('DB_name', 'simoncini_636010');
 define('DB_port', 3306);
-*/
 ?>
